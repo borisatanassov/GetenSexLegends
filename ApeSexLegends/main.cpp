40a566067928220d2907b24866db4b772f8b2180
borisatanassov <48214285+borisatanassov@users.noreply.github.com>
2021-09-06 14:45:11 +0300

    temporary commit

--- ApeSexLegends/main.cpp
+++ ApeSexLegends/main.cpp
@@ -202,6 +202,7 @@ int main(int argc, char* argv[]) {
 	bool fastIceCollision = false;
 	bool getPlayerPosition = false;
 
+	bool tempB = false;
 	bool monkeRangePunching = false;
 	/// TITLE SCREEN
 	while (!startGame) {
